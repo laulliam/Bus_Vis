ece49569e8098731baa66eb319d2f70e0200cdc4
Liang Liu <1113408083@qq.com>
2018-01-20 19:49:55 +0800

    add mongodb  --Liang Liu

--- routes/index.js
+++ routes/index.js
@@ -32,6 +32,35 @@ var server= http.createServer(function(req,res){
         }
     });
 }).listen(8888);*/
+
+var MongoClient = require('mongodb').MongoClient;
+var DB_CONN_STR = 'mongodb://localhost:27017/traffic_data';
+
+router.get('/all_routes', function(req, res, next) {
+    var selectData = function(db, callback) {
+        //连接到表
+        var collection = db.collection('all_routes');
+        //查询数据
+        var whereStr = {}
+        collection.find().toArray(function(err, result) {
+            if(err)
+            {
+                console.log('Error:'+ err);
+                return;
+            }
+            callback(result);
+        });
+    }
+
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        selectData(db, function(result) {
+            res.json(result);
+            db.close();
+        });
+    });
+
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
