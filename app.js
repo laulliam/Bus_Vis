ab98662474b5215e247c69d245fea743dea9f656
Liang Liu <1113408083@qq.com>
2018-01-20 21:02:31 +0800

    station data ajax   --Liang Liu

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var station = require('./routes/station')
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/',station)
 app.use('/users', users);
 
 // catch 404 and forward to error handler
